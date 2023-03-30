256d60a2244d361634a27bc524946fec44e64b24
Jaewon Chung <jaewonchung97@gmail.com>
2023-03-31 00:21:28 +0900

    First Commit

---
+++ lib/widgets/button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final Color bgColor;
+  final Color textColor;
+
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.bgColor,
+    required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 50,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 22,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
